b1c2ec47f20155268d00785b81eea9526888d8ae
Aryav Bhola <69618788+aryav-022@users.noreply.github.com>
2022-11-06 03:02:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request
+from os import listdir
+import cv2
+from flask_cors import CORS
+
+
+def compare(test_path, data1path, data2path):
+    # test image
+    image = cv2.imread(test_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
+
+    # data1 image
+    image = cv2.imread(data1path)
+    gray_image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    histogram1 = cv2.calcHist([gray_image1], [0], None, [256], [0, 256])
+
+    # data2 image
+    image = cv2.imread(data2path)
+    gray_image2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    histogram2 = cv2.calcHist([gray_image2], [0], None, [256], [0, 256])
+
+
+    c1, c2 = 0, 0
+
+    # Euclidean Distance between data1 and test
+    i = 0
+    while i < len(histogram) and i < len(histogram1):
+        c1 += (histogram[i]-histogram1[i])**2
+        i += 1
+    c1 = c1**(1 / 2)
+
+
+    # Euclidean Distance between data2 and test
+    i = 0
+    while i < len(histogram) and i < len(histogram2):
+        c2 += (histogram[i]-histogram2[i])**2
+        i += 1
+    c2 = c2**(1 / 2)
+
+    if (c1 < c2):
+        return data1path
+    else:
+        return data2path
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def upload_file():
+    return render_template(r'./upload.html')
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        file_name = f'temp.{f.filename.split(".")[-1]}'
+        f.save(file_name)
+
+        best_path = "./not_found/not_found.jpg"
+
+        folders = listdir('./photo')
+        for folder in folders:
+            images = listdir(f'./photo/{folder}')
+            for image in images:
+                image_path = f'./photo/{folder}/{image}'
+                best_path = compare(file_name, image_path, best_path)
+
+        relative_img_path = "." + best_path
+        person_name = best_path.split('/')[-2]
+
+        return render_template(r'./dashboard.html', name=person_name, img_path=relative_img_path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
